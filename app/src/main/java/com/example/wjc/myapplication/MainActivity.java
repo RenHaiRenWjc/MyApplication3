c65f6dc27e472462f0903b95699851093b9e5dae
chenwangjun <wjchenpl@foxmail.com>
2019-06-04 08:29:39 +0800

    类加载

--- app/src/main/java/com/example/wjc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wjc/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wjc.myapplication;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.LinearGradient;
@@ -9,68 +10,29 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Shader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import dalvik.system.DexClassLoader;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.iv1)
-    ImageView mIv1;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-//        Bitmap bitmap = Bitmap.createBitmap(500, 200, Bitmap.Config.ARGB_4444);
-//        Paint paint = new Paint();
-//        paint.setColor(this.getResources().getColor(R.color.colorPrimary));
-//        Canvas canvas = new Canvas(bitmap);
-//        canvas.drawBitmap(bitmap, 0, 0, paint);  //绘制原图
-//        //3.画笔使用LinearGradient 线性渐变渲染
-//        LinearGradient lg = new LinearGradient(0, 10, 0, bitmap.getHeight(),
-////                0x1Aff0000, 0x00ffff00, Shader.TileMode.MIRROR);
-//                0x33261c45, 0x00000000, Shader.TileMode.MIRROR);
-//        paint.setShader(lg);
-//        paint.setColor(Color.RED);
-//
-//        //4.指定画笔的Xfermode 即绘制的模式（不同的模式，绘制的区域不同）
-//        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_ATOP));
-//        paint.setAntiAlias(true);
-//        //5.在倒立图区，绘制矩形渲染图层
-//        canvas.drawRect(0, 20, 500, bitmap.getHeight(), paint);
-//        paint.setXfermode(null);
-//        mIv1.setImageBitmap(createGradientBitmap(R.mipmap.ic_launcher));
+        ClassLoader classLoader = getClassLoader();
+        ClassLoader classLoader1 = Activity.class.getClassLoader();
+        Log.i(TAG, "onCreate: getClassLoader=" + classLoader);
+        Log.i(TAG, "onCreate: getClassLoader 父类：" + classLoader.getParent());
+        Log.i(TAG, "onCreate:Activity classLoader1=" + classLoader1);
     }
 
 
-    @OnClick({R.id.bt_progressBar})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.bt_progressBar:
-//                startActivity(new Intent(this, ProgressBarActivity.class));
-                break;
-        }
-    }
-
-    private Bitmap createGradientBitmap(Bitmap bmp) {
-        int w = bmp.getWidth();
-        int h = bmp.getHeight();
-
-        Bitmap b = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(b);
-
-        Paint paint = new Paint();
-        paint.setShader(new LinearGradient(0, 0, 0, h, 0xffffffff, 0x00ffffff, Shader.TileMode.CLAMP));
-        canvas.drawBitmap(bmp, 0, 0, null);
-
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-        canvas.drawRect(0, 0, w, h, paint);
-
-        return b;
-    }
 }
